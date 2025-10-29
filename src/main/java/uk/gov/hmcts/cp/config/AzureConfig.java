dd83ce6a93e1f7805d3009b55406546d87e17d3e
Arcadius Ahouansou <arcadius@menelic.com>
2025-10-29 06:29:39 +0000

    deployment fix

--- src/main/java/uk/gov/hmcts/cp/config/AzureConfig.java
+++ src/main/java/uk/gov/hmcts/cp/config/AzureConfig.java
@@ -1,8 +1,15 @@
 package uk.gov.hmcts.cp.config;
 
+import static java.lang.String.format;
+
+import java.time.Duration;
+
+import com.azure.core.util.ConfigurationBuilder;
+import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -16,18 +23,25 @@ import org.springframework.context.annotation.Configuration;
     matchIfMissing = false
 )
 public class AzureConfig {
-
-    @Value("${azure.storage.connection-string}")
-    private String connectionString;
+    public static final String AZURE_CLIENT_ID = "AZURE_CLIENT_ID";
+    public static final String AZURE_TENANT_ID = "AZURE_TENANT_ID";
 
     @Value("${azure.storage.container-name}")
     private String containerName;
 
+    @Value( "${azure.client.id}")
+    private String clientId;
+
+    @Value("${azure.tenant.id}")
+    private String tenantId;
+
+    @Value("${azure.storage.account.name}")
+    private String storageAccountName;
+
+
     @Bean
     public BlobContainerClient blobContainerClient() {
-        BlobServiceClient serviceClient = new BlobServiceClientBuilder()
-                .connectionString(connectionString)
-                .buildClient();
+        BlobServiceClient serviceClient = createBlobServiceClient();
 
         BlobContainerClient containerClient = serviceClient.getBlobContainerClient(containerName);
 
@@ -36,5 +50,22 @@ public class AzureConfig {
         }
 
         return containerClient;
+    }
+
+    private BlobServiceClient createBlobServiceClient() {
+
+        final com.azure.core.util.Configuration configuration = new ConfigurationBuilder()
+                .putProperty(AZURE_CLIENT_ID, clientId)
+                .putProperty(AZURE_TENANT_ID, tenantId)
+                .build();
+
+        return new BlobServiceClientBuilder()
+                .endpoint(format("https://%s.blob.core.windows.net/", storageAccountName))
+                .credential(new DefaultAzureCredentialBuilder()
+                        .tenantId(tenantId)
+                        .managedIdentityClientId(clientId)
+                        .configuration(configuration)
+                        .build())
+                .buildClient();
     }
 }
