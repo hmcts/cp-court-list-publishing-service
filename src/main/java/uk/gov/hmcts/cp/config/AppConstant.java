1a7ada28aec31d7a02955d27dbfc96fc770d57d2
Arcadius Ahouansou <arcadius@menelic.com>
2026-09-09 15:07:45 +0100

    sjp publishing

--- src/main/java/uk/gov/hmcts/cp/config/AppConstant.java
+++ src/main/java/uk/gov/hmcts/cp/config/AppConstant.java
@@ -1,10 +1,11 @@
 package uk.gov.hmcts.cp.config;
 
 public class AppConstant {
-    private AppConstant(){}
-
     public static final String CJSCPPUID = "CJSCPPUID";
 
     /** Log marker for alerting on publish failures; used across both the standard and SJP flows. */
     public static final String ALERT_PATTERN = "PUBLISHING_FAILED";
+
+    private AppConstant(){}
+
 }
--- src/main/java/uk/gov/hmcts/cp/controllers/CourtListPublishController.java
+++ src/main/java/uk/gov/hmcts/cp/controllers/CourtListPublishController.java
@@ -28,7 +28,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpHeaders;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
